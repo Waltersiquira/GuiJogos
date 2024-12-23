4d4ae388611ab9f090e120b997f29c6e3c84112c
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-23 11:23:35 -0300

    Update add.php

--- add.php
+++ add.php
@@ -7,10 +7,10 @@
 </head>
 <body>
     <form action="enviar.php" method="post">
-        digite o nome do seu jogo: <input type="text"> <br>
-        digite o preço do seu jogo: <input type="text"> <br>
-        digite a descrição do seu jogo: <input type="text"> <br>
-        digite a imagem do seu jogo: <input type="text"> <br>
+        digite o nome do seu jogo: <input type="text" name="nome"> <br>
+        digite o preço do seu jogo: <input type="text" name="preco"> <br>
+        digite a descrição do seu jogo: <input type="text" name="descricao"> <br>
+        digite a imagem do seu jogo: <input type="text" name="imagem"> <br>
         <input type="submit">
     </form>
 </body>
