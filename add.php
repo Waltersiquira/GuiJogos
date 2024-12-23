e1778d500df8f7f14802252a041bd43629579e96
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-23 11:19:57 -0300

    Create add.php

---
+++ add.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiJogos</title>
+</head>
+<body>
+    <form action="enviar.php" method="post">
+        digite o nome do seu jogo: <input type="text"> <br>
+        digite o preço do seu jogo: <input type="text"> <br>
+        digite a descrição do seu jogo: <input type="text"> <br>
+        digite a imagem do seu jogo: <input type="text"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
